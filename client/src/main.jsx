bc0e40633fc12bd940f0e82de8560f7c2db1e103
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-12-19 22:01:16 -0600

    Restart

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import Home from './Home/Home'
+import Home from './pages/Home'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold text-black">Aryaman Ramchandran</h1>
+      <p className="text-xl text-black mt-2">portfolio</p>
+    </div>
+  )
+}
+
+export default Home
