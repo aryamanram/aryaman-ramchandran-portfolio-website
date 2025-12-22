0a7a8ab8d45a62cd6bfe3d84d7e272791b847209
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-12-21 21:45:46 -0600

    text and coloring

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold text-black">Aryaman Ramchandran</h1>
-      <p className="text-xl text-black mt-2">portfolio</p>
+      <h1 className="text-6xl font-bold text-black">Aryaman Ramchandran</h1>
+      <p className="text-2xl text-black mt-3">portfolio</p>
     </div>
   )
 }
